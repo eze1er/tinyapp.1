f73ffe8ad6baafd9f48627da2a8682d4f93bb01f
eze1er <papaezechiel@gmail.com>
2022-01-27 14:36:24 +0000

    getUserByEmail added

---
+++ helpers.js
@@ -0,0 +1,11 @@
+
+const getUserByEmail = (email, database) => {
+  for (const user in database) {
+    if (database[user].email === email) {
+      return database[user];
+    }
+  }
+  return null;
+};
+
+module.exports = { getUserByEmail };
